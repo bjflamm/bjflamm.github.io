7ee76d1a6a3482f53c961846a06f98bbf664bcad
bjflamm <flamm.benjamin@gatech.edu>
2018-07-22 00:08:55 -0400

    midnight updates!!!

--- js/image.js
+++ js/image.js
@@ -1,9 +1,9 @@
-// Get the modal
+
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
-var modalImg = document.getElementById(img);
+var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
